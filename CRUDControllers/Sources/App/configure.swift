cfbbfb8d78ee98c253236d7a31a707ce18558535
ShengHua Wu <fantasy0404@gmail.com>
2018-07-22 18:38:38 +0200

    [CRUD] Remove unused files.

--- CRUDControllers/Sources/App/configure.swift
+++ CRUDControllers/Sources/App/configure.swift
@@ -13,7 +13,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
@@ -27,7 +26,5 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
-
 }
