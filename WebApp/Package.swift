90bc40cf9b0500fb9fc04454b3a857da5a811044
ShengHua Wu <fantasy0404@gmail.com>
2018-10-14 17:16:24 +0200

    [Web] Add Leaf dependency.

--- WebApp/Package.swift
+++ WebApp/Package.swift
@@ -6,10 +6,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc")
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Authentication"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Authentication", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
