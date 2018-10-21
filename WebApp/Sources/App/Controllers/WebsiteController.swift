76051f79c0c5050cd640c72fb1c5d2dcb6d86391
ShengHua Wu <fantasy0404@gmail.com>
2018-10-21 16:14:22 +0200

    [Web] Rewrite index page and create user detail page.

--- WebApp/Sources/App/Controllers/WebsiteController.swift
+++ WebApp/Sources/App/Controllers/WebsiteController.swift
@@ -3,10 +3,34 @@ import Leaf
 
 final class WebsiteController: RouteCollection {
     func boot(router: Router) throws {
-        router.get("vapor", use: indexHandler)
+        let usersRoute = router.grouped("vapor")
+        
+        usersRoute.get(use: indexHandler)
+        usersRoute.get("users", User.parameter, use: userHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
-        return try req.view().render("index")
+        return User.query(on: req).decode(data: User.Public.self).all().flatMap(to: View.self) { users in
+            let publicUsers = users.isEmpty ? nil : users
+            let content = IndexContent(title: "Homepage", users: publicUsers)
+            return try req.view().render("index", content)
+        }
     }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(User.self).toPublic().flatMap(to: View.self) { publicUser in
+            let content = UserContext(title: publicUser.name, user: publicUser)
+            return try req.view().render("user", content)
+        }
+    }
+}
+
+struct IndexContent: Encodable {
+    let title: String
+    let users: [User.Public]?
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User.Public
 }
