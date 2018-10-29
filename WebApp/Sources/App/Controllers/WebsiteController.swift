89dbe6b8eb4bdd6d1590458bb0ce212c34739abd
ShengHuaWu <fantasy0404@gmail.com>
2018-10-29 16:55:28 +0100

    [Web] Append all pets and pet pages.

--- WebApp/Sources/App/Controllers/WebsiteController.swift
+++ WebApp/Sources/App/Controllers/WebsiteController.swift
@@ -8,6 +8,8 @@ final class WebsiteController: RouteCollection {
         websiteRoute.get(use: indexHandler)
         websiteRoute.get("users", use: allUsersHandler)
         websiteRoute.get("users", User.parameter, use: userHandler)
+        websiteRoute.get("pets", use: allPetsHandler)
+        websiteRoute.get("pets", Pet.parameter, use: petHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -27,8 +29,24 @@ final class WebsiteController: RouteCollection {
         return try req.parameters.next(User.self).flatMap(to: View.self) { user in
             return try user.pets.query(on: req).all().flatMap(to: View.self) { pets in
                 let publicUser = user.toPublic()
-                let content = UserContext(title: publicUser.name, user: publicUser, pets: pets)
+                let content = UserContent(title: publicUser.name, user: publicUser, pets: pets)
                 return try req.view().render("user", content)
+            }
+        }
+    }
+    
+    func allPetsHandler(_ req: Request) throws -> Future<View> {
+        return Pet.query(on: req).decode(Pet.self).all().flatMap(to: View.self) { pets in
+            let content = AllPetsContent(title: "All Pets", pets: pets)
+            return try req.view().render("allPets", content)
+        }
+    }
+    
+    func petHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Pet.self).flatMap(to: View.self) { pet in
+            return pet.user.get(on: req).flatMap(to: View.self) { user in
+                let content = PetContent(title: pet.name, pet: pet, user: user.toPublic())
+                return try req.view().render("pet", content)
             }
         }
     }
@@ -43,8 +61,19 @@ struct AllUsersContent: Encodable {
     let users: [User.Public]?
 }
 
-struct UserContext: Encodable {
+struct UserContent: Encodable {
     let title: String
     let user: User.Public
     let pets: [Pet]
+}
+
+struct AllPetsContent: Encodable {
+    let title: String
+    let pets: [Pet]
+}
+
+struct PetContent: Encodable {
+    let title: String
+    let pet: Pet
+    let user: User.Public
 }
