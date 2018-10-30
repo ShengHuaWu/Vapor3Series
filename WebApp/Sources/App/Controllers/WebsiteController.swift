f5ef77c79d26d37479344715be872073f307a0ff
ShengHuaWu <fantasy0404@gmail.com>
2018-10-30 15:39:30 +0100

    [Web] Append categories and category pages.

--- WebApp/Sources/App/Controllers/WebsiteController.swift
+++ WebApp/Sources/App/Controllers/WebsiteController.swift
@@ -10,6 +10,8 @@ final class WebsiteController: RouteCollection {
         websiteRoute.get("users", User.parameter, use: userHandler)
         websiteRoute.get("pets", use: allPetsHandler)
         websiteRoute.get("pets", Pet.parameter, use: petHandler)
+        websiteRoute.get("categories", use: allCategoriesHandler)
+        websiteRoute.get("categories", Category.parameter, use: categoryHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -50,6 +52,22 @@ final class WebsiteController: RouteCollection {
             }
         }
     }
+    
+    func allCategoriesHandler(_ req: Request) throws -> Future<View> {
+        return Category.query(on: req).decode(Category.self).all().flatMap(to: View.self) { categories in
+            let content = AllCategoriesContent(title: "All Categories", categories: categories)
+            return try req.view().render("allCategories", content)
+        }
+    }
+    
+    func categoryHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Category.self).flatMap(to: View.self) { category in
+            return try category.pets.query(on: req).all().flatMap(to: View.self) { pets in
+                let content = CategoryContent(title: category.name, category: category, pets: pets)
+                return try req.view().render("category", content)
+            }
+        }
+    }
 }
 
 struct IndexContent: Encodable {
@@ -76,4 +94,15 @@ struct PetContent: Encodable {
     let title: String
     let pet: Pet
     let user: User.Public
+}
+
+struct AllCategoriesContent: Encodable {
+    let title: String
+    let categories: [Category]
+}
+
+struct CategoryContent: Encodable {
+    let title: String
+    let category: Category
+    let pets: [Pet]
 }
